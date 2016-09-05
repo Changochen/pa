61cdaadfabd9465a48bb2eab0db9280c55ac6e24
tracer-ics2016 <tracer@njuics.org>
2016-09-05 12:57:50 +0000

    >  compile NEMU 151220012 chango Linux ee148ae4b61e 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 GNU/Linux  12:57:50 up  2:17,  1 user,  load average: 0.19, 0.12, 0.09 48668d8a34944c6269c474654006357e5f7528c5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -70,7 +70,8 @@ static int cmd_x(char *args)
    printf("0x%x:\t",addr.addr_u);
    int i=1;
    for(;i<=k;i++){
-    printf("%x",(unsigned int)*(addr.addr_p++));
+     char c=*(addr.addr_p++);
+    printf("%x",c);
     if(i%4==0)printf("\t");
     if(i%12==0)printf("\n\t");
    }
