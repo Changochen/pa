4122b4b5faa3e1735caf704f60d10bbbb32343a2
tracer-ics2016 <tracer@njuics.org>
2016-09-19 09:06:25 +0000

    >  compile NEMU 151220012 chango Linux ee148ae4b61e 4.4.0-31-generic #50-Ubuntu SMP Wed Jul 13 00:07:12 UTC 2016 x86_64 GNU/Linux  09:06:25 up  4:43,  1 user,  load average: 0.11, 0.06, 0.08 b2caac5b8a3b1a1465d4781de9f92fb4f397e9a

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -19,5 +19,32 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(){
+	if(free_==NULL)assert(0);
+	else{
+		WP* new_p=free_;
+		free_=free_->next;
+		new_p->next=head;
+		head=new_p;
+		return new_p;
+	}
+}
 
-
+void free_wp(WP *wp){
+	if(wp==NULL)return;
+	else{
+		if(head==wp){
+			head=head->next;
+			wp->next=free_;
+			free_=wp;
+			return;
+		}
+		WP *temp=head;
+		while(temp->next!=NULL&&temp->next!=wp)temp=temp->next;
+		if(temp->next==NULL)assert(0);
+		WP *freetemp=temp->next;
+		temp->next=temp->next->next;	
+		freetemp->next=free_;
+		free_=freetemp;
+	}
+}
